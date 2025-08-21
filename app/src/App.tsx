a9d1ad6d35c732dd0bb737a499e39923cb0fb5ae
hazuki3417 <hazuki3417@gmail.com>
2025-08-21 16:05:09 +0900

    srcから回転させる実装のサンプルコードを追加

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Providers } from "./Providers";
-import ImageRotateAndDownload from "./ImageRotate";
+import ImageRotateAndDownload2 from "./ImageRotate2";
 
 export default function App() {
 	return (
 		<Providers>
 			{/* 下記に配置するコンポーネントを適宜入れ替えて検証をするイメージで利用する */}
 			example
-			<ImageRotateAndDownload />
+			<ImageRotateAndDownload2 />
 		</Providers>
 	);
 }
