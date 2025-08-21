7b960b45b7848f8cb10f805b2bd81c85f20d92d6
hazuki3417 <hazuki3417@gmail.com>
2025-08-21 15:19:41 +0900

    検証コード追加

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Providers } from "./Providers";
+import ImageRotateAndDownload from "./ImageRotate";
 
 export default function App() {
 	return (
 		<Providers>
 			{/* 下記に配置するコンポーネントを適宜入れ替えて検証をするイメージで利用する */}
 			example
+			<ImageRotateAndDownload />
 		</Providers>
 	);
 }
