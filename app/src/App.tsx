69bd81ef8a569cc68e3786d8f32b78faa71509ac
hazuki3417 <hazuki3417@gmail.com>
2025-08-25 15:44:38 +0900

    検証コードをビルド除外ディレクトリへ移動

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Providers } from "./Providers";
-import ImageRotateAndDownload2 from "./ImageRotate2";
 
 export default function App() {
 	return (
 		<Providers>
 			{/* 下記に配置するコンポーネントを適宜入れ替えて検証をするイメージで利用する */}
 			example
-			<ImageRotateAndDownload2 />
 		</Providers>
 	);
 }
