f0676a63959ac6155a0c62950e1912bb3f920fde
hazuki3417 <hazuki3417@gmail.com>
2025-11-10 16:08:03 +0900

    コメント追加

--- app/src/components/InputSelect/InputSelectOption.tsx
+++ app/src/components/InputSelect/InputSelectOption.tsx
@@ -2,19 +2,25 @@ import React, { forwardRef } from "react";
 import styled from "styled-components";
 import { type StyledProps } from "@/utils/props";
 
-interface StyleProps {}
+interface StyleProps { }
 
 const Base = styled.option<StyledProps<StyleProps>>`
 `;
 
 type BaseProps = React.ComponentPropsWithoutRef<"option">;
 
-export interface InputSelectOptionProps extends StyleProps, BaseProps {}
+export interface InputSelectOptionProps extends StyleProps, BaseProps { }
 
 export const InputSelectOption = forwardRef<
   HTMLOptionElement,
   InputSelectOptionProps
 >((props, ref) => {
   const { ...rest } = props;
+
+  /**
+   * NOTE: 現状の実装では装飾なしで実装
+   *       optionに対する装飾が必要になったときは随時拡張する
+   */
+
   return <Base ref={ref} {...rest} />;
 });
