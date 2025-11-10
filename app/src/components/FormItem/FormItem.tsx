6e700ec8089bf20cbfb15cdc16f2e2b8da31462b
hazuki3417 <hazuki3417@gmail.com>
2025-11-10 20:51:11 +0900

    コメント追加

--- app/src/components/FormItem/FormItem.tsx
+++ app/src/components/FormItem/FormItem.tsx
@@ -5,6 +5,7 @@ import { FormItemLabel } from "./FormitemLabel";
 import { FormItemDescription } from "./FormItemDescription";
 import { FormItemMessage } from "./FormItemMessage";
 import { FormItemContainer } from "./FormItemContainer";
+import { transform } from "@/utils/props";
 
 type BaseProps = React.HTMLAttributes<HTMLDivElement>;
 
@@ -17,7 +18,7 @@ const Base = styled.div`
   gap: 4px
 `;
 
-interface StyleProps {}
+interface StyleProps { }
 
 export interface FormItemProps extends StyleProps, BaseProps {
   children?: React.ReactNode;
@@ -32,10 +33,28 @@ type FormItemComponent = React.ForwardRefExoticComponent<
   Message: typeof FormItemMessage;
 };
 
+/**
+ * フォームアイテムを構成するコンポーネントです。（レイアウト・装飾のみを提供します）
+ * @example
+ * <FormItem>
+ *   <FormItem.Label>図面番号</FormItem.Label>
+ *   <FormItem.Container>
+ *     <InputText placeholder="例：12345-67890" />
+ *   </FormItem.Container>
+ *   <FormItem.Message>図面番号の入力は必須です</FormItem.Message>
+ *   <FormItem.Description>
+ *     図面番号が衝突した場合、確認のダイアログが表示されます。
+ *   </FormItem.Description>
+ * </FormItem>
+ */
 export const FormItem = forwardRef<HTMLDivElement, FormItemProps>(
   (props, ref) => {
     const { ...rest } = props;
-    return <Base ref={ref} {...rest} />;
+    // key: value -> $key: value に変換($をkey名の先頭に付与)
+    const styled = transform.props.toStyled({
+      // NOTE: スタイルプロップが追加された場合はここに追記
+    });
+    return <Base ref={ref} {...styled} {...rest} />;
   },
 ) as FormItemComponent;
 
